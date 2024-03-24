60e0e67bbb259265dd7d2535911a41ec6c4872e6
Moniruzzaman Sujan <157636081+Mjsujan@users.noreply.github.com>
2024-03-24 08:24:49 +0000

    add 2 file veriable & dataTyper

---
+++ 01_basics/01_veriable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "admin@gmail.com"
+var accountPassword = "123456"
+/*accountCity = "panchagarh" */
+let accountStat;
+
+
+// accountId = 25 
+
+// In modern world dont use var
+
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountStat])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  its not working cozz We are working node.js 
+
+console.log(3+3);
+
+let name = "Sujan"
+let age = 65
+let isLoggIn = false;
+
+
+/*
+number => 2
+bigint 
+string =""
+Boolean = true/false
+null = empty
+undefined = ;
+symbol = unique;
+*/
+
+console.log(typeof "Sujann");
