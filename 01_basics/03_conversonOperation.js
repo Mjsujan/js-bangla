757929b6cb8b3b899374a3c8978893c026ed0fd1
Moniruzzaman Sujan <157636081+Mjsujan@users.noreply.github.com>
2024-03-24 09:00:05 +0000

    add conversionOperation file

---
+++ 01_basics/03_conversonOperation.js
@@ -0,0 +1,22 @@
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let convertType = Number(score);
+// console.log(typeof(convertType));
+//"33" => 33
+//"33sdfh" => NsN
+//ture= 1 false = 
+
+let isadalt = 0;
+let booleanAdalt = Boolean(isadalt);
+console.log(typeof(booleanAdalt));
+console.log(booleanAdalt);
+
+//  1 => true, 0 => False;
+
+let isadalts = 33;
+let stringAdalt = String(isadalts);
+console.log(typeof(stringAdalt));
+console.log(stringAdalt);
